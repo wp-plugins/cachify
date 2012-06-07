5ae3d54b5767481afc45f757c2ab359422b1ad82
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-07 15:29:05 +0000

    Add previous plugin versions
    git-svn-id: https://plugins.svn.wordpress.org/cachify/tags/1.4@554373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ apc/proxy.php
@@ -0,0 +1,16 @@
+<?php
+if (
+	extension_loaded('apc')
+	&& ( !empty($_SERVER['PHP_SELF']) && strpos($_SERVER['PHP_SELF'], '/wp-admin/') === false )
+	&& ( !empty($_SERVER['HTTP_ACCEPT_ENCODING']) && strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false )
+	&& ( $cache = apc_fetch('cachify_' .md5($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])) )
+) {
+	header('Vary: Accept-Encoding');
+	header('X-Powered-By: Cachify');
+	header('Content-Encoding: gzip');
+	header('Content-Length: '.strlen($cache));
+	header('Content-Type: text/html; charset=utf-8');
+
+    echo $cache;
+    exit;
+}
